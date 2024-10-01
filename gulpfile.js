801b1ec7fbd296f533c2a8574e8127b50b3ae6ea
noobwebdev777 <webdev.proj.777@gmail.com>
2024-10-01 21:23:46 +0530

    correction done

--- gulpfile.js
+++ gulpfile.js
@@ -1,293 +1,27 @@
-// const { src, dest, watch, series, parallel } = require('gulp');
-// const sass = require("gulp-sass")(require("sass"));
-// const browserSync = require("browser-sync").create();
-// const autoprefixer = require("gulp-autoprefixer");
-// const cleanCSS = require("gulp-clean-css");
-// const rename = require("gulp-rename");
-
-// // Compile Sass, Autoprefix, Minify, and Uglify CSS
-// function compileSass() {
-//   return src("./src/scss/**/*.scss")
-//     .pipe(sass().on("error", sass.logError))
-//     .pipe(autoprefixer({
-//       cascade: false
-//     }))
-//     .pipe(dest("./public/css")) // Save non-minified CSS
-//     .pipe(cleanCSS()) // Minify CSS
-//     .pipe(rename({ suffix: ".min" })) // Rename to .min.css
-//     .pipe(dest("./src/css")) // Save minified CSS
-//     .pipe(browserSync.stream()); // Inject CSS changes
-// }
-
-// // New Prettier task using dynamic import
-// async function prettify() {
-//   const { default: prettier } = await import("gulp-prettier");
-//   return src([
-//     "./*.html",
-//     "./src/js/**/*.js",
-//     "./src/scss/**/*.scss",
-//     "./src/css/**/*.css",
-//   ])
-//   .pipe(prettier({ singleQuote: true }))
-//   .pipe(dest((file) => file.base));
-// }
-
-// // Serve and watch HTML/Sass/image files
-// function serve(cb) {
-//   // Initialize BrowserSync
-//   browserSync.init({
-//     server: {
-//       baseDir: "./", // Serve files from the root folder
-//     },
-//     port: 5173,
-//     open: false, // Prevent BrowserSync from opening a new browser window
-//     notify: false, // Optional: Disable notifications in the browser
-//   });
-
-//   // Watch HTML, CSS, and JS files for changes
-//   watch("./*.html", series(prettify, browserSync.reload));
-//   watch("./src/css/*.css", series(prettify, browserSync.reload));
-//   watch("./src/js/*.js", series(prettify, browserSync.reload));
-
-//   // Watch Sass files for changes
-//   watch("./src/scss/**/*.scss", series(compileSass, prettify));
-
-//   // Watch image files for changes
-//   watch("./src/images/**/*.{jpg,jpeg,png,gif}", browserSync.reload);
-
-//   cb();
-// }
-
-// // Default task
-// exports.default = series(compileSass, prettify, serve);
-
-
-//-------------------------------------------------------
-
-// const { src, dest, watch, series } = require('gulp');
-// const sass = require("gulp-sass")(require("sass"));
-// const browserSync = require("browser-sync").create();
-// const autoprefixer = require("gulp-autoprefixer");
-// const cleanCSS = require("gulp-clean-css");
-// const rename = require("gulp-rename");
-
-// // Compile Sass, Autoprefix, Minify, and Rename CSS
-// function compileSass() {
-//   return src("./src/scss/**/*.scss")
-//     .pipe(sass().on("error", sass.logError))
-//     .pipe(autoprefixer({
-//       cascade: false
-//     }))
-//     .pipe(dest("./public/css")) // Save non-minified CSS
-//     .pipe(cleanCSS()) // Minify CSS
-//     .pipe(rename({ suffix: ".min" })) // Rename to .min.css
-//     .pipe(dest("./public/css")) // Save minified CSS
-//     .pipe(browserSync.stream()); // Inject CSS changes
-// }
-
-// // Prettify task using dynamic import
-// async function prettify() {
-//   const { default: prettier } = await import("gulp-prettier");
-//   return src([
-//     "./*.html",
-//     "./src/js/**/*.js",
-//     "./src/scss/**/*.scss",
-//     "./src/css/**/*.css",
-//   ])
-//   .pipe(prettier({ singleQuote: true }))
-//   .pipe(dest((file) => file.base)); // Save prettified files
-// }
-
-// // Serve and watch files
-// function serve() {
-//   // Initialize BrowserSync
-//   browserSync.init({
-//     server: {
-//       baseDir: "./", // Serve files from the root folder
-//     },
-//     port: 5173,
-//     open: false, // Prevent BrowserSync from opening a new browser window
-//     notify: false, // Optional: Disable notifications in the browser
-//   });
-
-//   // Watch HTML, CSS, and JS files for changes
-//   watch("./*.html", series(prettify, browserSync.reload));
-//   watch("./src/css/*.css", series(prettify, browserSync.reload));
-//   watch("./src/js/*.js", series(prettify, browserSync.reload));
-
-//   // Watch Sass files for changes
-//   watch("./src/scss/**/*.scss", series(compileSass, prettify));
-
-//   // Watch image files for changes
-//   watch("./src/images/**/*.{jpg,jpeg,png,gif}", browserSync.reload);
-// }
-
-// Serve and watch files
-
-// function serve() {
-//   // Initialize BrowserSync
-//   browserSync.init({
-//     server: {
-//       baseDir: "./", // Serve files from the root folder
-//     },
-//     port: 5173,
-//     open: false, // Prevent BrowserSync from opening a new browser window
-//     notify: false, // Optional: Disable notifications in the browser
-//   });
-
-//   // Watch HTML files for changes
-//   watch("./*.html", series(prettify, (done) => {
-//       browserSync.reload();
-//       done();
-//   }));
-
-//   // Watch JS files for changes
-//   watch("./src/js/**/*.js", series(prettify, (done) => {
-//       browserSync.reload();
-//       done();
-//   }));
-
-//   // Watch CSS files for changes
-//   watch("./src/css/*.css", (done) => {
-//       browserSync.reload();
-//       done();
-//   });
-
-//   // Watch Sass files for changes
-//   watch("./src/scss/**/*.scss", series(compileSass, prettify));
-
-//   // Watch image files for changes
-//   watch("./src/images/**/*.{jpg,jpeg,png,gif}", browserSync.reload);
-// }
-
-
-// // Default task
-// exports.default = series(compileSass, prettify, serve);
-
-
-
-//-------------------------------------------------------
-
-// ... rest of your Gulp tasks ...
-
-// const { src, dest, watch, series } = require('gulp');
-// const sass = require("gulp-sass")(require("sass"));
-// const browserSync = require("browser-sync").create();
-// const autoprefixer = require("gulp-autoprefixer");
-// const cleanCSS = require("gulp-clean-css");
-// const rename = require("gulp-rename");
-
-// // Compile Sass, Autoprefix, Minify, and Rename CSS
-
-// function compileSass() {
-//   return src("./src/scss/**/*.scss")
-//     .pipe(sass().on("error", sass.logError))
-//     .pipe(autoprefixer({
-//       cascade: false
-//     }))
-//     .pipe(dest("./public/css")) // Save non-minified CSS
-//     .pipe(cleanCSS()) // Minify CSS
-//     .pipe(rename({ suffix: ".min" })) // Rename to .min.css
-//     .pipe(dest("./public/css")) // Save minified CSS
-//     .pipe(browserSync.stream()); // Inject CSS changes
-// }
-
-// // Prettify task using dynamic import
-// async function prettify() {
-//   const { default: prettier } = await import("gulp-prettier");
-//   return src([
-//     "./*.html",
-//     "./src/js/**/*.js",
-//     "./src/scss/**/*.scss",
-//     // Exclude minified CSS
-//     "./src/css/*.css", // Make sure this doesn't include minified CSS files
-//   ])
-//   .pipe(prettier({ singleQuote: true }))
-//   //.pipe(dest((file) => file.base)); // Save prettified files
-// }
-
-// // Serve and watch files
-// function serve() {
-//   // Initialize BrowserSync
-//   browserSync.init({
-//     server: {
-//       baseDir: "./", // Serve files from the root folder
-//     },
-//     port: 5173,
-//     open: false, // Prevent BrowserSync from opening a new browser window
-//     notify: false, // Optional: Disable notifications in the browser
-//   });
-
-//   // Watch HTML files for changes
-//   watch("./*.html", series(prettify, (done) => {
-//       browserSync.reload();
-//       done();
-//   }));
-
-//   // Watch JS files for changes
-//   watch("./src/js/**/*.js", series(prettify, (done) => {
-//       browserSync.reload();
-//       done();
-//   }));
-
-//   // Watch CSS files for changes, excluding minified ones
-//   watch("./src/css/*.css", (done) => {
-//       browserSync.reload();
-//       done();
-//   });
-
-//   // Watch Sass files for changes
-//   watch("./src/scss/**/*.scss", series(compileSass, prettify));
-
-//   // Watch image files for changes
-//   watch("./src/images/**/*.{jpg,jpeg,png,gif}", browserSync.reload);
-// }
-
-// // Default task
-// exports.default = series(compileSass, prettify, serve);
-
-
-//-------------------------------------------------------
-
-//chatGPT help my profile
-
-const { src, dest, watch, series } = require('gulp');
+const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const browserSync = require("browser-sync").create();
 const autoprefixer = require("gulp-autoprefixer");
 const cleanCSS = require("gulp-clean-css");
 const rename = require("gulp-rename");
 
-// Compile Sass, Autoprefix, Minify, and Rename CSS
-function compileSass() {
-  return src("./src/scss/**/*.scss")
+// Compile Sass, Autoprefix, Minify, and Uglify CSS
+gulp.task("sass", function () {
+  return gulp
+    .src("./src/scss/**/*.scss")
     .pipe(sass().on("error", sass.logError))
     .pipe(autoprefixer({
       cascade: false
     }))
-    .pipe(dest("./public/css")) // Save non-minified CSS
+    .pipe(gulp.dest("./public/css")) // Save non-minified CSS
     .pipe(cleanCSS()) // Minify CSS
     .pipe(rename({ suffix: ".min" })) // Rename to .min.css
-    .pipe(dest("./public/css")) // Save minified CSS
+    .pipe(gulp.dest("./public/css")) // Save minified CSS
     .pipe(browserSync.stream()); // Inject CSS changes
-}
+});
 
-// Prettify task using dynamic import
-async function prettify() {
-  const { default: prettier } = await import("gulp-prettier");
-  return src([
-    "./*.html",
-    "./src/js/**/*.js",
-    "./src/scss/**/*.scss",
-    // Exclude minified CSS
-    "!./public/css/*.min.css", // Exclude minified CSS
-  ])
-  .pipe(prettier({ singleQuote: true }))
-  //.pipe(dest((file) => file.base)); // Uncomment if you want to save prettified files
-}
-
-// Serve and watch files
-function serve() {
+// Serve and watch HTML/Sass/image files
+gulp.task("serve", function () {
   // Initialize BrowserSync
   browserSync.init({
     server: {
@@ -298,29 +32,17 @@ function serve() {
     notify: false, // Optional: Disable notifications in the browser
   });
 
-  // Watch HTML files for changes
-  watch("./*.html", series(prettify, (done) => {
-    browserSync.reload();
-    done();
-  }));
-
-  // Watch JS files for changes
-  watch("./src/js/**/*.js", series(prettify, (done) => {
-    browserSync.reload();
-    done();
-  }));
+  // Watch HTML, CSS, and JS files for changes
+  gulp.watch("./*.html").on("change", browserSync.reload);
+  gulp.watch("./src/css/*.css").on("change", browserSync.reload);
+  gulp.watch("./src/js/*.js").on("change", browserSync.reload);
 
   // Watch Sass files for changes
-  watch("./src/scss/**/*.scss", series(compileSass, prettify));
+  gulp.watch("./src/scss/**/*.scss", gulp.series("sass"));
 
   // Watch image files for changes
-  watch("./src/images/**/*.{jpg,jpeg,png,gif}", (done) => {
-    browserSync.reload();
-    done();
-  });
-}
+  gulp.watch("./src/images/**/*.{jpg,jpeg,png,gif}", browserSync.reload);
+});
 
 // Default task
-exports.default = series(compileSass, prettify, serve);
-
-
+gulp.task("default", gulp.series("sass", "serve"));
---
+++ src/js/main.js
@@ -0,0 +1,2 @@
+console.log('setup done')
+        console.log(12 + 69)
